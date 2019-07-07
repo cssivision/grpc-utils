3bad4380053d9adff286fbfc16d483099d971e5a
cssivision <cssivision@gmail.com>
2019-07-07 19:57:00 +0800

    add start method

--- go.mod
+++ go.mod
@@ -1,3 +1,36 @@
 module github.com/cssivision/grpc-utils
 
 go 1.12
+
+require (
+	cloud.google.com/go v0.41.0 // indirect
+	github.com/coreos/bbolt v1.3.3 // indirect
+	github.com/coreos/etcd v3.3.13+incompatible // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190620071333-e64a0ec8b42a // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.3 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/prometheus/client_golang v1.0.0 // indirect
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/etcd v3.3.13+incompatible
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/exp v0.0.0-20190627132806-fd42eb6b336f // indirect
+	golang.org/x/image v0.0.0-20190703141733-d6a02ce849c9 // indirect
+	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
+	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/tools v0.0.0-20190706070813-72ffa07ba3db // indirect
+	google.golang.org/genproto v0.0.0-20190701230453-710ae3a149df // indirect
+	google.golang.org/grpc v1.22.0
+)
--- resolver/resolver.go
+++ resolver/resolver.go
@@ -1,1 +1,90 @@
 package resolver
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"google.golang.org/grpc/resolver"
+)
+
+// GrpcResolverBuilder ...
+type GrpcResolverBuilder struct {
+	client *clientv3.Client
+}
+
+// Build ...
+func (b *GrpcResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOption) (resolver.Resolver, error) {
+	r := &GrpcResolver{
+		client:   b.client,
+		cc:       cc,
+		endpoint: target.Endpoint,
+	}
+
+	if err := r.start(); err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// Scheme ...
+func (*GrpcResolverBuilder) Scheme() string {
+	return "discovery"
+}
+
+// GrpcResolver ...
+type GrpcResolver struct {
+	client   *clientv3.Client
+	cc       resolver.ClientConn
+	endpoint string
+}
+
+// Update ...
+type Update struct {
+	// address
+	Addr string
+	// meta data
+	Metadata map[string]interface{}
+}
+
+func (gr *GrpcResolver) start() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	resp, err := gr.client.Get(ctx, gr.endpoint+"/", clientv3.WithPrefix(), clientv3.WithSerializable())
+	if err != nil {
+		log.Printf("start get from etcd err: %s\n", err.Error())
+		return err
+	}
+
+	addrs := make([]resolver.Address, 0, len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		var jupdate Update
+		if err := json.Unmarshal(kv.Value, &jupdate); err != nil {
+			log.Printf("unmarshal update err: %s\n", err.Error())
+			continue
+		}
+		addrs = append(addrs, resolver.Address{
+			Addr:     jupdate.Addr,
+			Metadata: jupdate.Metadata,
+		})
+	}
+
+	gr.cc.UpdateState(resolver.State{
+		Addresses: addrs,
+	})
+	return nil
+}
+
+func (*GrpcResolver) watch() {
+
+}
+
+// Close ...
+func (*GrpcResolver) Close() {}
+
+// ResolveNow ...
+func (*GrpcResolver) ResolveNow(o resolver.ResolveNowOption) {}

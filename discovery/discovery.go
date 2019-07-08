8ad46d94cafb9c01c43acb994cb0a6739061dc7a
cssivision <cssivision@gmail.com>
2019-07-08 21:03:51 +0800

    discovery get routes

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -1,11 +1,88 @@
 package discovery
 
+import (
+	"context"
+	"encoding/json"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/clientv3"
+)
+
+// Discovery ...
+type Discovery struct {
+	client *clientv3.Client
+	mutex  sync.RWMutex
+	store  map[string]*Registry
+}
+
+// Registry ...
+type Registry struct {
+	Addresses []*Address
+	client    *clientv3.Client
+}
+
+// Address ...
 type Address struct {
-	Addr string 
+	Addr     string
 	Metadata interface{}
 }
 
-// GetAllRoutes ...
-func GetAllRoutes(appid string) []*Address {
-	return nil 
+// GetRoutes ...
+func (ds *Discovery) GetRoutes(appid string) ([]*Address, error) {
+	ds.mutex.RLock()
+	r, ok := ds.store[appid]
+	ds.mutex.RUnlock()
+	if !ok {
+		ds.mutex.Lock()
+		nr, err := NewRegistry(ds.client, appid)
+		if err != nil {
+			logrus.Error(err.Error())
+		}
+		ds.store[appid] = nr
+		ds.mutex.Unlock()
+		return nr.Addresses, nil
+	}
+
+	return r.Addresses, nil
+}
+
+// Update ...
+type Update struct {
+	// address
+	Addr string
+	// meta data
+	Metadata map[string]interface{}
+	// op
+	Op int
+}
+
+// NewRegistry ...
+func NewRegistry(client *clientv3.Client, appid string) (*Registry, error) {
+	target := appid + "/"
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	resp, err := client.Get(ctx, target, clientv3.WithPrefix(), clientv3.WithSerializable())
+	if err != nil {
+		logrus.Error(err.Error())
+		return nil, err
+	}
+	addrs := make([]*Address, 0, len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		var jupdate Update
+		if err := json.Unmarshal(kv.Value, &jupdate); err != nil {
+			logrus.Errorf("unmarshal update err: %s\n", err.Error())
+			continue
+		}
+		addrs = append(addrs, &Address{
+			Addr:     jupdate.Addr,
+			Metadata: jupdate.Metadata,
+		})
+	}
+
+	return &Registry{
+		client:    client,
+		Addresses: addrs,
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -16,8 +16,11 @@ require (
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.9.3 // indirect
 	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/prometheus/client_golang v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.etcd.io/etcd v3.3.13+incompatible
